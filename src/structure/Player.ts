86e603f0af5520a09d79b454013414318914658a
raziman <raziman@tutanota.com>
2022-03-04 21:25:14 +0800

    throw new error when character has not been set

--- src/structure/Player.ts
+++ src/structure/Player.ts
@@ -9,6 +9,10 @@ export class Player extends Entity {
     const data = client.players.get(user.id);
     const player = new Player(user.id, user.username, user.displayAvatarURL());
 
+    if (!data) {
+      throw new Error("Character has not been created. Please run `!create`");
+    }
+
     Object.assign(player, data);
 
     player.name = user.username;
