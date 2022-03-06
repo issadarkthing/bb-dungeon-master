3a14a641d70139ea2721ff60be3f073ec5f96c94
raziman <raziman@tutanota.com>
2022-03-06 21:26:02 +0800

    add role settings

---
+++ src/commands/Event.ts
@@ -0,0 +1,23 @@
+import { Command } from "@jiman24/commandment";
+import { Message, PermissionResolvable } from "discord.js";
+import { client } from "..";
+
+export default class extends Command {
+  name = "event";
+  description = "set roles that can initiate event or dungeon";
+  permissions: PermissionResolvable[] = ["ADMINISTRATOR"];
+
+  async exec(msg: Message) {
+
+    const roles = [...msg.mentions.roles.values()];
+
+    if (roles.length === 0) {
+      throw new Error("you need to mention at least 1 role");
+    }
+
+    client.settings.set("role-events", roles);
+
+    msg.channel.send(`Successfully set roles`);
+
+  }
+}
--- src/structure/Client.ts
+++ src/structure/Client.ts
@@ -2,7 +2,10 @@ import { CommandManager } from "@jiman24/commandment";
 import { Client as DiscordClient } from "discord.js";
 import Enmap from "enmap";
 
+type Settings = "role-events";
+
 export class Client extends DiscordClient {
   commandManager = new CommandManager(process.env.PREFIX || "!");
   players = new Enmap("player");
+  settings = new Enmap<Settings, any>("settings");
 }
